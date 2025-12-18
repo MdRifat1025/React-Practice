e9a557e63e355f076bce868229860a8445cf664e
RifatSarker <rifatsarkercse@gmail.com>
2025-12-18 16:59:44 +0600

    push assignment-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Card from "./components/Card.jsx"
+
+import Product from "./assignment-1/product.jsx"
 
 
 function App() {
@@ -11,7 +14,7 @@ function App() {
 
   return (
     <>
-      <Card text="hello world" />
+      <Product />
     </>
   )
 }
---
+++ src/assignment-1/product.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./product.css";
+import product from "./prodcut.json";
+
+function Product() {
+    return (
+        <div className="container mt-4">
+            <div className="row">
+                {product.map((item) => (
+                    <div className="col-md-4 mb-4" key={item.id}>
+                        <div className="card h-100">
+                            <div className="card-body">
+                                <h5 className="card-title">{item.name}</h5>
+                                <h6 className="card-subtitle mb-2 text-muted">
+                                    {item.category}
+                                </h6>
+                                <p className="card-text">Price: {item.price}</p>
+                                <p className="card-text">
+                                    {item.inStock ? "In Stock" : "Out of Stock"}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default Product;
