06e91836be2c6fc43f347d818ace4cf11755add7
RifatSarker <rifatsarkercse@gmail.com>
2025-12-24 16:11:54 +0600

    togole

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Parent from './state_lifting/Parent.jsx';
 import Home from './state_lifting/Home.jsx';
 import Form from './form/Form.jsx';
+import Toggle from './Toggle/Toggle.jsx';
 
 function App() {
 
@@ -9,10 +10,12 @@ function App() {
   return (
     <>
       {/* <Child data={data} onChildData={getData} /> */}
-      <Form />
+      <Toggle />
 
     </>
   );
 }
 
 export default App;
+
+
