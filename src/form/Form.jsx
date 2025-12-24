300aaade96d721e27020579d8ea8f1bedccdef84
RifatSarker <rifatsarkercse@gmail.com>
2025-12-24 13:59:56 +0600

    yup

--- src/form/Form.jsx
+++ src/form/Form.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 
 export default function Form() {
@@ -10,10 +11,24 @@ export default function Form() {
             email: '',
             pass: ''
         },
+        validationSchema: Yup.object({
+            name: Yup.string()
+                .min(3, 'Minimum 3 characters')
+                .required('Name is required'),
+
+            email: Yup.string()
+                .email('Invalid email format')
+                .required('Email is required'),
+
+            pass: Yup.string()
+                .min(6, 'Password must be at least 6 characters')
+                .required('Password is required')
+        }),
         onSubmit: (values) => {
             console.log(values);
         }
     })
+    console.log(formik.errors);
 
     // const [name, setName] = useState('');
     // const [email, setEmail] = useState('');
@@ -53,14 +68,23 @@ export default function Form() {
                 <label style={{ color: 'blue', margin: '10px' }}>Username:</label>
 
                 <input onChange={formik.handleChange} style={{ margin: '10px' }} type="text" value={formik.values.name} name="name" /><br />
+                {formik.touched.name && formik.errors.name && (
+                    <div style={{ color: 'red' }}>{formik.errors.name}</div>
+                )}
 
                 <label style={{ color: 'blue', margin: '10px' }}>Email:</label>
 
                 <input onChange={formik.handleChange} style={{ margin: '10px' }} type="text" value={formik.values.email} name="email" /><br />
+                {formik.touched.name && formik.errors.name && (
+                    <div style={{ color: 'red' }}>{formik.errors.email}</div>
+                )}
 
                 <label style={{ color: 'blue', margin: '10px' }}>Passwprd</label>
 
                 <input onChange={formik.handleChange} style={{ margin: '10px' }} type="text" value={formik.values.pass} name="pass" /><br />
+                {formik.touched.name && formik.errors.name && (
+                    <div style={{ color: 'red' }}>{formik.errors.pass}</div>
+                )}
 
                 <button style={{ backgroundColor: "green", color: 'white' }} type="submit">
                     Submit
